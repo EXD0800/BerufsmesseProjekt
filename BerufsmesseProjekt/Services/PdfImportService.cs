adb33a1e5073c91c48fa05f2923eebca841d56ef
Oliver Schiwek <oliver.schiwek@panther-packaging.de>
2025-06-24 16:01:35 +0200

    Anpassungen

--- BerufsmesseProjekt/Services/PdfImportService.cs
+++ BerufsmesseProjekt/Services/PdfImportService.cs
@@ -2,11 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UglyToad.PdfPig;
 using System.Threading.Tasks;
 
 namespace BerufsmesseProjekt.Services;
 
 public class PdfImportService
 {
+    public void Import()
+    {
+        Console.WriteLine("Bitte geben Sie den Dateipfad an:");
+        string path = Console.ReadLine();
 
+        foreach (var file in Directory.GetFiles(path))
+        {
+
+        }
+    }
 }
