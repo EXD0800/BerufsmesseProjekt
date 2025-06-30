e6e754f5f3ec997cbd8795937d3bc41847f567a5
Oliver Schiwek <oliver.schiwek@panther-packaging.de>
2025-06-30 09:56:10 +0200

    Firmenmodel hinzugefügt

---
+++ BerufsmesseProjekt/Models/FirmenModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BerufsmesseProjekt.Models;
+
+public class FirmenModel
+{
+    public string Firmenname { get; set; }
+    public string Ausbildungsberuf { get; set; }
+    public string Branche { get; set; }
+}
