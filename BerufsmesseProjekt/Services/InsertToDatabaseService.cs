ba344fe58bc7e8958b35b798dffbb2b6f9ee040c
Oliver Schiwek <oliver.schiwek@panther-packaging.de>
2025-07-01 13:37:20 +0200

    Databaseimporter angelegt

---
+++ BerufsmesseProjekt/Services/InsertToDatabaseService.cs
@@ -0,0 +1,16 @@
+﻿using BerufsmesseProjekt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BerufsmesseProjekt.Services;
+
+public class InsertToDatabaseService
+{
+    public void InsertPDFToDatabase(List<PdfModel> pdfContent)
+    {
+
+    }
+}
